a844ccaf58100099d83e4573194ce1fbb12d2e80
chiahong <chiahong@pop-os.localdomain>
2021-07-15 14:43:47 +0800

    add styles feature

--- wordpress/wp-content/themes/first_theme/gulpfile.babel.js
+++ wordpress/wp-content/themes/first_theme/gulpfile.babel.js
@@ -2,14 +2,28 @@ import gulp from 'gulp';
 import yargs from 'yargs';
 import cleanCSS from 'gulp-clean-css';
 import gulpIf from 'gulp-if';
+import sourceMaps from 'gulp-sourcemaps';
 const sass = require('gulp-sass')(require('sass'));
 
 const PRODUCTION = yargs.argv.prod;
 
+const paths = {
+  styles: {
+    src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+    dest: 'dist/assets/css',
+  },
+};
+
 export const styles = () => {
   return gulp
-    .src('src/assets/scss/bundle.scss')
+    .src(paths.styles.src)
+    .pipe(gulpIf(!PRODUCTION, sourceMaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpIf(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
-    .pipe(gulp.dest('dist/assets/css'));
+    .pipe(gulpIf(!PRODUCTION, sourceMaps.write()))
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+export const watch = () => {
+  gulp.watch('src/assets/scss/**/*.scss', styles);
 };
--- wordpress/wp-content/themes/first_theme/lib/enqueue-assets.php
+++ wordpress/wp-content/themes/first_theme/lib/enqueue-assets.php
@@ -11,3 +11,15 @@ function firsttheme_assets() {
 }
 
 add_action('wp_enqueue_scripts', 'firsttheme_assets');
+
+function firsttheme_admin_assets() {
+	wp_enqueue_style(
+		'firsttheme-style', 
+		get_template_directory_uri() . '/dist/assets/css/bundle.css',
+		array(),
+		'1.0.0',
+		'all'
+	);
+}
+
+add_action('admin_enqueue_scripts', 'firsttheme_assets');
