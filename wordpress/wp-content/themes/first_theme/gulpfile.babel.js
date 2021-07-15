f51b91d4d9d0b9d0ed618e7d2288df7a15e38b17
chiahong <chiahong@pop-os.localdomain>
2021-07-15 14:21:35 +0800

    gulp setup

--- wordpress/wp-content/themes/first_theme/gulpfile.babel.js
+++ wordpress/wp-content/themes/first_theme/gulpfile.babel.js
@@ -1,11 +1,15 @@
-// import gulp from 'gulp';
+import gulp from 'gulp';
 import yargs from 'yargs';
+import cleanCSS from 'gulp-clean-css';
+import gulpIf from 'gulp-if';
+const sass = require('gulp-sass')(require('sass'));
 
 const PRODUCTION = yargs.argv.prod;
 
-export const hello = done => {
-  console.log(PRODUCTION);
-  done();
+export const styles = () => {
+  return gulp
+    .src('src/assets/scss/bundle.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulpIf(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
+    .pipe(gulp.dest('dist/assets/css'));
 };
-
-export default hello;
