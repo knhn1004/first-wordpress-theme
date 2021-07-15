7877f96b9507065dc2187bbcc0d7376fc664d998
chiahong <chiahong@pop-os.localdomain>
2021-07-15 13:22:29 +0800

    add escaping

--- wordpress/wp-content/themes/first_theme/lib/helpers.php
+++ wordpress/wp-content/themes/first_theme/lib/helpers.php
@@ -1,19 +1,19 @@
 <?php
 
 function firsttheme_post_meta() {
-			echo 'Posted on ';
-			echo '<a href="' . get_permalink() . '">';
-			echo '<time datetime="' .  get_the_date() . '">'
-				. get_the_date()
-			. '</time>';
-			echo '</a>';
-			echo ' By <a href="' . get_author_posts_url(get_the_author_meta('ID'))  . '">';
-			echo get_the_author();
-			echo '</a>';
+	echo 'Posted on ';
+	echo '<a href="' . esc_url(get_permalink()) . '">';
+	echo '<time datetime="' .  esc_attr(get_the_date()) . '">'
+		. esc_html(get_the_date())
+	. '</time>';
+	echo '</a>';
+	echo ' By <a href="' . esc_url(get_author_posts_url(get_the_author_meta('ID')))  . '">';
+	echo esc_html(get_the_author());
+	echo '</a>';
 }
 
 function  firsttheme_readmore_link() {
-	echo '<a href="' . get_the_permalink() . '" title="' . the_title_attribute(['echo' => false]) . '">';
+	echo '<a href="' . esc_url(get_the_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
 	echo 'Read More <span class="u-screen-reader-text">';
 	echo 'About ' . the_title();
 	echo '</span>';
