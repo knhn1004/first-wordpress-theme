ff9526364807f8b6ed5722ca833ddf4324076f49
oliver <oliver@pop-os.localdomain>
2021-07-16 07:20:04 +0800

    enqueue admin scripts

--- wordpress/wp-content/themes/first_theme/lib/enqueue-assets.php
+++ wordpress/wp-content/themes/first_theme/lib/enqueue-assets.php
@@ -13,6 +13,7 @@ function firsttheme_assets() {
 		'firsttheme-scripts',
 		get_template_directory_uri() . '/dist/assets/js/bundle.js',
 		array(),
+		'1.0.0',
 		true
 	);
 }
@@ -27,6 +28,14 @@ function firsttheme_admin_assets() {
 		'1.0.0',
 		'all'
 	);
+
+	wp_enqueue_script(
+		'firsttheme-admin-scripts',
+		get_template_directory_uri() . '/dist/assets/js/admin.js',
+		array(),
+		'1.0.0',
+		true
+	);
 }
 
-add_action('admin_enqueue_scripts', 'firsttheme_assets');
+add_action('admin_enqueue_scripts', 'firsttheme_admin_assets');
