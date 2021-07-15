a91aaa9b0c819b4aaf907ced92b5b714f8850935
chiahong <chiahong@pop-os.localdomain>
2021-07-15 13:10:03 +0800

    update functions

--- wordpress/wp-content/themes/first_theme/functions.php
+++ wordpress/wp-content/themes/first_theme/functions.php
@@ -8,15 +8,15 @@ function post_meta() {
 			. '</time>';
 			echo '</a>';
 			echo 'By <a href="' . get_author_posts_url(get_the_author_meta('ID'))  . '">';
-				 echo get_the_author();
+			echo get_the_author();
 			echo '</a>';
 }
 
 function  readmore_link() {
 	echo '<a href="' . get_the_permalink() . '" title="' . the_title_attribute().  '">';
-		echo 'Read More <span class="u-screen-reader-text">';
-				echo 'About ' . the_title();
-			echo '</span>';
-		echo '</a>';
+	echo 'Read More <span class="u-screen-reader-text">';
+	echo 'About ' . the_title();
+	echo '</span>';
+	echo '</a>';
 
 }
