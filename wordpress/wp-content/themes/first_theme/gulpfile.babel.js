a18511df1058897542747f85bcf13a04028870d9
chiahong <chiahong@pop-os.localdomain>
2021-07-15 15:21:06 +0800

    setup gulp image compression

--- wordpress/wp-content/themes/first_theme/gulpfile.babel.js
+++ wordpress/wp-content/themes/first_theme/gulpfile.babel.js
@@ -3,6 +3,7 @@ import yargs from 'yargs';
 import cleanCSS from 'gulp-clean-css';
 import gulpIf from 'gulp-if';
 import sourceMaps from 'gulp-sourcemaps';
+import imageMin from 'gulp-imagemin';
 const sass = require('gulp-sass')(require('sass'));
 
 const PRODUCTION = yargs.argv.prod;
@@ -11,6 +12,18 @@ const paths = {
   styles: {
     src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
     dest: 'dist/assets/css',
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+    dest: 'dist/assets/images',
+  },
+  other: {
+    src: [
+      'src/assets/**/*',
+      '!src/assets/{images,js,scss}',
+      '!src/assets/{images,js,scss}/**/*',
+    ],
+    dest: 'dist/assets',
   },
 };
 
@@ -22,6 +35,17 @@ export const styles = () => {
     .pipe(gulpIf(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
     .pipe(gulpIf(!PRODUCTION, sourceMaps.write()))
     .pipe(gulp.dest(paths.styles.dest));
+};
+
+export const images = () => {
+  return gulp
+    .src(paths.images.src)
+    .pipe(gulpIf(PRODUCTION, imageMin()))
+    .pipe(gulp.dest(paths.images.dest));
+};
+
+export const copy = () => {
+  return gulp.src(paths.other.src).pipe(gulp.dest(paths.other.dest));
 };
 
 export const watch = () => {
