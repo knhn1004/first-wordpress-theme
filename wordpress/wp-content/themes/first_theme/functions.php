a2e49b399c8d7341a5495643600f1f01ac92c89d
oliver <oliver@pop-os.localdomain>
2021-07-16 08:36:19 +0800

    wordpress core actions

--- wordpress/wp-content/themes/first_theme/functions.php
+++ wordpress/wp-content/themes/first_theme/functions.php
@@ -16,3 +16,14 @@ function after_pagination2()
 add_action('_themename_after_pagination', 'after_pagination', 2);
 add_action('_themename_after_pagination', 'after_pagination2', 1);
 
+add_action('pre_get_posts', 'function_to_add', 10, 1);
+
+function function_to_add($query)
+{
+    if ($query->is_main_query()) {
+        $query->set('posts_per_page', 2);
+    }
+}
+
+remove_action('pre_get_posts', 'function_to_add', 10, 1);
+
