2559f4444fadcc70a13932bbaa1008cca77479dc
chiahong <chiahong@pop-os.localdomain>
2021-07-16 09:32:04 +0800

    filter

--- wordpress/wp-content/themes/first_theme/functions.php
+++ wordpress/wp-content/themes/first_theme/functions.php
@@ -25,5 +25,15 @@ function function_to_add($query)
     }
 }
 
-remove_action('pre_get_posts', 'function_to_add', 10, 1);
+function no_posts_text($text) {
+    // return $text . ' TEST';
+    return esc_html__('No Posts', 'shwjgjs-child');
+}
 
+add_filter('_themename_no_posts_text', 'no_posts_text');
+
+function filter_title($title) {
+    return 'Filtered ' . $title;
+}
+
+add_filter('the_title', 'filter_title');
--- wordpress/wp-content/themes/first_theme/index.php
+++ wordpress/wp-content/themes/first_theme/index.php
@@ -20,7 +20,7 @@
 
 <?php } else { ?>
 	<p>
-	<?php esc_html_e('Sorry, no post was found', '_themename'); ?>
+	<?php echo apply_filters('_themename_no_posts_text', esc_html('Sorry, no post was found', '_themename')) ; ?>
 	</p>
 <?php } ?>
 
