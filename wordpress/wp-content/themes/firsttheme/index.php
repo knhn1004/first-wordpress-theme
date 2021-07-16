51bd2e173294be9d85831533be463187a26bfc3c
chiahong <chiahong@pop-os.localdomain>
2021-07-16 09:58:42 +0800

    child theme

---
+++ wordpress/wp-content/themes/firsttheme/index.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+<?php if (have_posts()) { ?>
+	<?php while (have_posts()) { ?>
+		<?php the_post(); ?>
+		<h2>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<?php the_title(); ?>
+		</a>
+		</h2>
+		<div>
+			<?php _themename_post_meta(); ?>
+		</div>
+		<div>
+			<?php the_excerpt(); ?>
+		</div>
+		<?php _themename_readmore_link(); ?>
+	<?php } ?>
+	<?php the_posts_pagination(); ?>
+	<?php do_action('_themename_after_pagination'); ?>
+
+<?php } else { ?>
+	<p>
+	<?php echo apply_filters('_themename_no_posts_text', esc_html__('Sorry, no post was found', '_themename')) ; ?>
+	</p>
+<?php } ?>
+
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/firsttheme/lib/helpers.php
@@ -0,0 +1,52 @@
+<?php
+
+if (!function_exists('_themename_post_meta')) {
+    function _themename_post_meta()
+    {
+        /* translators: %s: Post Date */
+        printf(
+            esc_html('Posted on %s', '_themename'),
+            '<a href="' .
+                esc_url(get_permalink()) .
+                '<time datetime="' .
+                esc_attr(get_the_date()) .
+                '">' .
+                esc_html(get_the_date()) .
+                '</time></a>'
+        );
+
+        /* translators: %s: Author */
+        printf(
+            esc_html__(' By %s', '_themename'),
+            '<a href="' .
+                esc_url(get_author_posts_url(get_the_author_meta('ID'))) .
+                '">' .
+                esc_html(get_the_author()) .
+                '</a>'
+        );
+    }
+}
+
+function _themename_readmore_link()
+{
+    echo '<a href="' .
+        esc_url(get_the_permalink()) .
+        '" title="' .
+        the_title_attribute(['echo' => false]) .
+        '">';
+    /* translators: %s: Post Title */
+    printf(
+        wp_kses(
+            __(
+                'Read More <span class="u-screen-reader-text">About %',
+                '_themename'
+            ),
+            [
+                'span' => [
+                    'class' => [],
+                ],
+            ],
+            get_the_title() . '</span></a>'
+        )
+    );
+}
