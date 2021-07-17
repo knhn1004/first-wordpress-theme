c957d553901a85a6565ec9122ecf6e1511210544
oliver <oliver@pop-os.localdomain>
2021-07-18 01:46:25 +0800

    creating a simple plugin

---
+++ wordpress/wp-content/plugins/_themename-metaboxes/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: _themename _pluginname
+Plugin URI:
+Description: Adding Metaboxes for _themename
+Version: 1.0.0
+Author: Chiahong Chou
+Author URI: https://chunhongweb.com
+License: MIT
+Text Domain: _themename-pluginname
+Domain Path: /languages
+*/
+
+if (!defined('WPINC')) {
+    die();
+}
+
+?>
