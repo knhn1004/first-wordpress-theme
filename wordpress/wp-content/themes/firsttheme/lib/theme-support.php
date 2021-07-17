ef574a1aab1e4593ff01b2cecd30c02e7f2b0fe9
oliver <oliver@pop-os.localdomain>
2021-07-17 14:07:34 +0800

    updating customizer using selective refresh

--- wordpress/wp-content/themes/firsttheme/lib/theme-support.php
+++ wordpress/wp-content/themes/firsttheme/lib/theme-support.php
@@ -11,6 +11,7 @@ function _themename_theme_support()
         'gallery',
         'caption',
     ]);
+    add_theme_support('customize-selective-refresh-widgets');
 }
 
 add_action('after_setup_theme', '_themename_theme_support');
