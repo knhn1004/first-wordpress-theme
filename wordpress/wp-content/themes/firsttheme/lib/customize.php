b1209b46b547821a0a5d5558e0aec34ed2862935
oliver <oliver@pop-os.localdomain>
2021-07-17 13:35:33 +0800

    site info, theme option

---
+++ wordpress/wp-content/themes/firsttheme/lib/customize.php
@@ -0,0 +1,27 @@
+<?php
+
+function _themename_customize_register($wp_customize)
+{
+    $wp_customize->add_section('_themename_footer_options', [
+        'title' => esc_html__('Footer Options', '_themename'),
+        'description' => esc_html__(
+            'You can change footer options from here.',
+            '_themename'
+        ),
+        'priority' => 30,
+    ]);
+
+    $wp_customize->add_setting('_themename_site_info', [
+        'default' => '',
+        'sanitize_callback' => 'sanitize_text_field',
+    ]);
+    $wp_customize->add_control('_themename_site_info', [
+        'type' => 'text',
+        'label' => esc_html__('Site Info', '_themename'),
+        'section' => '_themename_footer_options',
+    ]);
+}
+
+add_action('customize_register', '_themename_customize_register');
+
+?>
