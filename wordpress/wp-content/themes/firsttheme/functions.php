5ef0a9240af7807df297365bd40f7a8352706520
oliver <oliver@pop-os.localdomain>
2021-07-18 00:36:39 +0800

    adding metaboxes & cutom fields

--- wordpress/wp-content/themes/firsttheme/functions.php
+++ wordpress/wp-content/themes/firsttheme/functions.php
@@ -6,5 +6,6 @@ require_once 'lib/sidebars.php';
 require_once 'lib/theme-support.php';
 require_once 'lib/navigation.php';
 require_once 'lib/delete-post.php';
+require_once 'lib/metaboxes.php';
 
 ?>
