6f17875a2a700c9f2f63f309b8cc70345c410608
oliver <oliver@pop-os.localdomain>
2021-07-18 03:27:56 +0800

    adding an option to show/hide author info

--- wordpress/wp-content/themes/firsttheme/lib/customize.php
+++ wordpress/wp-content/themes/firsttheme/lib/customize.php
@@ -13,6 +13,40 @@ function _themename_customize_register($wp_customize)
         },
     ]);
 
+    /*################## SINGLE SETTINGS ########################*/
+
+    $wp_customize->add_section('_themename_single_blog_options', [
+        'title' => esc_html__('Single Blog Options', '_themename'),
+        'description' => esc_html__(
+            'You can change single blog options from here.',
+            '_themename'
+        ),
+        'active_callback' => '_themename_show_single_blog_section',
+    ]);
+
+    $wp_customize->add_setting('_themename_display_author_info', [
+        'default' => true,
+        'transport' => 'postMessage',
+        'santize_callback' => '_themename_sanitize_checkbox',
+    ]);
+
+    $wp_customize->add_control('_themename_display_author_info', [
+        'type' => 'checkbox',
+        'label' => esc_html__('Show Author Info', '_themename'),
+        'section' => '_themename_single_blog_options',
+    ]);
+
+    function _themename_sanitize_checkbox($checked)
+    {
+        return isset($checked) && $checked;
+    }
+
+    function _themename_show_single_blog_section()
+    {
+        global $post;
+        return is_single() && $post->post_type === 'post';
+    }
+
     /*################## GENERAL SETTINGS ########################*/
     $wp_customize->add_section('_themename_general_options', [
         'title' => esc_html__('General Options', '_themename'),
