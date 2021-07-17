a5b6ef07b43f8bb9166b068e0d48ff4f8dcc150b
oliver <oliver@pop-os.localdomain>
2021-07-17 10:14:17 +0800

    adding markup for menu accessibility

--- wordpress/wp-content/themes/firsttheme/lib/navigation.php
+++ wordpress/wp-content/themes/firsttheme/lib/navigation.php
@@ -9,15 +9,43 @@ function _themename_register_menus()
 
 add_action('init', '_themename_register_menus');
 
+function _themename_aria_hasdropdown($atts, $item, $args)
+{
+    if ($args->theme_location == 'main-menu') {
+        if (in_array('menu-item-has-children', $item->classes)) {
+            $atts['aria-haspopup'] = 'true';
+            $atts['aria-expanded'] = 'false';
+        }
+    }
+    return $atts;
+}
+
+add_filter('nav_menu_link_attributes', '_themename_aria_hasdropdown', 10, 3);
+
+function _themename_submenu_button($dir = 'down', $title)
+{
+    $button = '<button class="menu-button">';
+    $button .=
+        '<span class="u-screen-reader-text menu-button-show">' .
+        sprintf(esc_html__('Show %s submenu', '_themename'), $title) .
+        '</span>';
+    $button .=
+        '<span aria-hidden="true" class="u-screen-reader-text menu-button-hide">' .
+        sprintf(esc_html__('Hide %s submenu', '_themename'), $title) .
+        '</span>';
+    $button .= '<i class="fa fa-angle-' . $dir . '" aria-hidden="true"></i>';
+    $button .= '</button>';
+    return $button;
+}
+
 function _themename_dropdown_icon($title, $item, $args, $depth)
 {
     if ($args->theme_location == 'main-menu') {
         if (in_array('menu-item-has-children', $item->classes)) {
             if ($depth == 0) {
-                $title .= '<i class="fa fa-angle-down" aria-hidden="true"></i>';
+                $title .= _themename_submenu_button('down', $title);
             } else {
-                $title .=
-                    '<i class="fa fa-angle-right" aria-hidden="true"></i>';
+                $title .= _themename_submenu_button('right', $title);
             }
         }
     }
