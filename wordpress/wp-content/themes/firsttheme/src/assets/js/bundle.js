41835f78c3691760c43a5517508b26961529976a
oliver <oliver@pop-os.localdomain>
2021-07-18 12:35:55 +0800

    gallery post format

--- wordpress/wp-content/themes/firsttheme/src/assets/js/bundle.js
+++ wordpress/wp-content/themes/firsttheme/src/assets/js/bundle.js
@@ -1,2 +1,13 @@
+import $ from 'jquery';
 import './components/slider';
 import './components/navigation';
+import 'slick-carousel';
+
+$(document).on('ready', () => {
+  $('.c-post__gallery, .c-post__gallery-gutenberg .wp-block-gallery').slick({
+    arrows: false,
+    adaptiveHeight: true,
+  });
+
+  $('.most_recent_widget').slick();
+});
