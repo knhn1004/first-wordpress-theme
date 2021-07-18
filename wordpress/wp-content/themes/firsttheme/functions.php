c55b67de2786e2152645742d3d49f8e549284120
oliver <oliver@pop-os.localdomain>
2021-07-18 08:02:45 +0800

    creating a simple shortcode

--- wordpress/wp-content/themes/firsttheme/functions.php
+++ wordpress/wp-content/themes/firsttheme/functions.php
@@ -9,4 +9,40 @@ require_once 'lib/delete-post.php';
 require_once 'lib/include-plugins.php';
 require_once 'lib/comment-callback.php';
 // require_once 'lib/metaboxes.php';
+
+function _themename_button($atts)
+{
+    extract(
+        shortcode_atts(
+            [
+                'color' => 'red',
+                'text' => 'Button',
+            ],
+            $atts
+        )
+    );
+    return '<button style="background-color: ' .
+        esc_attr($color) .
+        '">' .
+        esc_html__($text) .
+        '</button>';
+}
+
+add_shortcode('_themename_button', '_themename_button');
+
+function _themename_icon($atts)
+{
+    extract(
+        shortcode_atts(
+            [
+                'icon' => '',
+            ],
+            $atts
+        )
+    );
+    return '<i class="' . esc_attr($icon) . '" aria-hidden></i>';
+}
+
+add_shortcode('_themename_icon', '_themename_icon');
+
 ?>
