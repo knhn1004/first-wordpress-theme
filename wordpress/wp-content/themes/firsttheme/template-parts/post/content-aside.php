352d36f37c37e0259007b4fe03db09388924774e
oliver <oliver@pop-os.localdomain>
2021-07-18 11:34:53 +0800

    aside post format

--- wordpress/wp-content/themes/firsttheme/template-parts/post/content-aside.php
+++ wordpress/wp-content/themes/firsttheme/template-parts/post/content-aside.php
@@ -1,19 +1,15 @@
-aside
-<footer class="c-post__foter">
-<?php
-if (has_category()) {
-    echo '<div class="c-post__cats">';
-    /* translators: used between categories */
-    $cats_list = get_the_category_list(esc_html__(', ', '_themename'));
-    /* translators: %s is the categories list */
-    printf(esc_html__('Posted in %s', '_themename'), $cats_list);
-    echo '</div>';
-}
-if (has_tag()) {
-    echo '<div class="c-post__tags">';
-    $tags_list = get_the_tag_list('<ul><li>', '</li><li>', '</li></ul>');
-    echo $tags_list;
-    echo '</div>';
-}
-?>
-</footer>
+<article <?php post_class('c-post u-margin-bottom-20'); ?>>
+	<div class="c-post__inner">
+
+		<div class="c-post__content">
+			<?php the_content(); ?>
+		</div>
+		<div class="c-post__meta">
+			<?php _themename_post_meta(); ?>
+		</div>
+
+		<?php if (is_single()) { ?>
+		<?php get_template_part('template-parts/post/footer'); ?>
+		<?php } ?>
+	</div>
+</article>
